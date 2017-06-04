f666575094a9bb9e9c09b21024aa40f23c53c9ed
andrew oko-odion <mrniceguy4net@yahoo.com>
2017-06-04 21:36:39 +0100

    Add Comment Model

---
+++ web/models/comment.ex
@@ -0,0 +1,19 @@
+defmodule PhoenixBlog.Comment do
+  use PhoenixBlog.Web, :model
+
+  schema "comments" do
+    field :body, :string
+    belongs_to :post, PhoenixBlog.Post
+
+    timestamps()
+  end
+
+  @doc """
+  Builds a changeset based on the `struct` and `params`.
+  """
+  def changeset(struct, params \\ %{}) do
+    struct
+    |> cast(params, [:body])
+    |> validate_required([:body])
+  end
+end
